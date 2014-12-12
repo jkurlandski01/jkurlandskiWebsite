6803f514a5a10174a02e8e49e0c56bd4a1fe5b5c
jkurlandski <12356840+jkurlandski01@users.noreply.github.com>
2014-12-12 02:05:55 +0000

    Multimap.

---
+++ CodeExamples/src/deletable/MultimapTest.java
@@ -0,0 +1,105 @@
+package deletable;
+
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multiset;
+
+public class MultimapTest {
+
+	private Map<String, List<String>> getHashMap() {
+		Map<String, List<String>> map = Maps.newHashMap();
+		List<String> diveForms = Lists.newArrayList("dive", "dives", "dived");
+		map.put("dive", diveForms);
+		return map;
+	}
+
+	@Test
+	public void testOldWay() {
+		Map<String, List<String>> map = getHashMap();
+		List<String> diveForms = map.get("dive");
+		diveForms.add("dove");
+		map.put("dive", diveForms);
+		assertEquals(4, map.get("dive").size());
+		
+		List<String> moreDiveForms = Lists.newArrayList("diveth");
+		map.put("dive", moreDiveForms);
+		assertEquals(5, map.get("dive").size());		
+	}
+	
+	private Multimap<String, String> getMultimap() {
+		Multimap<String, String> mm = ArrayListMultimap.create();
+		List<String> diveForms = Lists.newArrayList("dive", "dives", "dived");
+		mm.putAll("dive", diveForms);
+		return mm;
+	}
+
+	@Test
+	public void testNewWay() {
+		Multimap<String, String> mm = getMultimap();
+		mm.put("dive", "dove");
+		assertEquals(4, mm.get("dive").size());
+		
+		mm.put("dive", "diveth");
+	}
+	
+	@Test
+	public void testMultimap() {
+		Multimap<String, String> mm = ArrayListMultimap.create();
+		mm.put("ask", "ask");
+		mm.put("ask", "asks");
+		mm.put("ask", "asked");
+		List<String> beList = Lists.newArrayList("am", "are", "is", "was", "were");
+		mm.putAll("be", beList);
+
+		// Iterate through all the entries
+		for(Entry<String, String> mapPair : mm.entries())  {
+		    String key = mapPair.getKey();
+		    String value = mapPair.getValue();
+		    System.out.println("key = " + key + "; value = " + value);
+		}
+
+		System.out.println("Write using toString():");
+		System.out.println(mm.toString());
+		
+		// Getting all entries when you don't care about the key.
+		Collection<String> valuesAsCollection = mm.values();
+		List<String> valuesAsList = Lists.newArrayList(valuesAsCollection);
+		Collections.sort(valuesAsList);
+		System.out.println(valuesAsList);
+		
+		// Testing the values for the key "be".
+		// containsKey( )
+		assertTrue(mm.containsKey("be"));
+		// get( )
+		Collection<String> beForms = mm.get("be");
+		assertEquals(5, beForms.size());
+		// containsEntry( )
+		assertTrue(mm.containsEntry("be", "was"));
+		// keySet( )
+		Set<String> keys = mm.keySet();
+		assertEquals(2, keys.size());
+		
+		
+		Multiset<String> multiset = mm.keys();
+		int size = multiset.size();
+		assertEquals(8, size);
+		assertEquals(5, multiset.count("be"));
+		assertEquals(3, multiset.count("ask"));
+		
+	}
+
+}
