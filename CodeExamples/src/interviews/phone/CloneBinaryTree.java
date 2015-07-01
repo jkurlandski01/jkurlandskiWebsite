474080303b253ed6bd631343eee945ac7cf66c64
jkurlandski <12356840+jkurlandski01@users.noreply.github.com>
2015-07-01 14:37:43 +0000

    New tree package. New Interviewee.java.

--- CodeExamples/src/interviews/phone/CloneBinaryTree.java
+++ CodeExamples/src/interviews/phone/CloneBinaryTree.java
@@ -1,5 +1,11 @@
 package interviews.phone;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+// Some of this code has been copied to tree.TreeNode.
+
 /**
  * Given a binary tree, do a mirror clone.
  * You cannot change the original tree.
@@ -16,7 +22,6 @@ public class CloneBinaryTree {
 
 	public TreeNode cloneTree(TreeNode root)	{
 		TreeNode newRoot = cloneNodeRecursive(root);
-		//TreeNode newRoot = cloneNodeNonRecursive(root);
 		
 		return newRoot;		
 	}
@@ -41,6 +46,72 @@ public class CloneBinaryTree {
 		return newNode;
 	}
 	
+	private void writeInOrderTraversal(TreeNode node)	{
+		if(node.left != null)	{
+			writeInOrderTraversal(node.left);
+		}
+		
+		write(node.val);
+		
+		if(node.right != null)	{
+			writeInOrderTraversal(node.right);
+		}
+	}
+
+	private void writePreOrderTraversal(TreeNode node)	{
+		write(node.val);
+		
+		if(node.left != null)	{
+			writeInOrderTraversal(node.left);
+		}
+		
+		if(node.right != null)	{
+			writeInOrderTraversal(node.right);
+		}
+	}
+
+	private void writePostOrderTraversal(TreeNode node)	{
+		if(node.left != null)	{
+			writeInOrderTraversal(node.left);
+		}
+		
+		if(node.right != null)	{
+			writeInOrderTraversal(node.right);
+		}
+
+		write(node.val);		
+	}
+
+	// This method doesn't work.
+	private void inOrderNonRecursiveTraversal(TreeNode root) {
+		TreeNode currNode = root;
+		TreeNode pre;
+		while(currNode != null)	{
+			if(currNode.left == null)	{
+				write(currNode.val);
+				currNode = currNode.right;
+			}
+			else	{
+				pre = currNode.left;
+				while(pre.right != null && pre.right != currNode)	{
+					pre = pre.right;
+					
+					if(pre.right == null)	{
+						pre.right = currNode;
+						currNode = currNode.left;
+					}
+					else	{
+						pre.right = null;
+						write(currNode.val);
+						currNode = currNode.right;
+					}
+				}
+			}
+		}
+	}
+
+
+	// Manually create a tree with hard-coded values.
 	private TreeNode createTree1()	{
 		TreeNode node2 = new TreeNode();
 		node2.val = 2;
@@ -79,88 +150,23 @@ public class CloneBinaryTree {
 		return root;
 	}
 	
-	private void inOrderTraversal(TreeNode node)	{
-		if(node.left != null)	{
-			inOrderTraversal(node.left);
-		}
-		
-		write(node.val);
-		
-		if(node.right != null)	{
-			inOrderTraversal(node.right);
-		}
-	}
-
-	private void preOrderTraversal(TreeNode node)	{
-		write(node.val);
-		
-		if(node.left != null)	{
-			inOrderTraversal(node.left);
-		}
-		
-		if(node.right != null)	{
-			inOrderTraversal(node.right);
-		}
-	}
-
-	private void postOrderTraversal(TreeNode node)	{
-		if(node.left != null)	{
-			inOrderTraversal(node.left);
-		}
-		
-		if(node.right != null)	{
-			inOrderTraversal(node.right);
-		}
-
-		write(node.val);		
-	}
-
 	private void write(int in)	{
 		System.out.println(in);
 	}
 	
-	// This method doesn't work.
-	private void inOrderNonRecursiveTraversal(TreeNode root) {
-		TreeNode currNode = root;
-		TreeNode pre;
-		while(currNode != null)	{
-			if(currNode.left == null)	{
-				write(currNode.val);
-				currNode = currNode.right;
-			}
-			else	{
-				pre = currNode.left;
-				while(pre.right != null && pre.right != currNode)	{
-					pre = pre.right;
-					
-					if(pre.right == null)	{
-						pre.right = currNode;
-						currNode = currNode.left;
-					}
-					else	{
-						pre.right = null;
-						write(currNode.val);
-						currNode = currNode.right;
-					}
-				}
-			}
-		}
-	}
-
-
-
 	public static void main(String[] args) {
 		CloneBinaryTree cbt = new CloneBinaryTree();
 		TreeNode node = cbt.createTree1();
 		
 		System.out.println("Printing inorder traversal of original:");
-		cbt.inOrderTraversal(node);
+		cbt.writeInOrderTraversal(node);
 		
 		TreeNode newNode = cbt.cloneNodeRecursive(node);
 
 		System.out.println("Printing inorder traversal of copy:");
-		cbt.inOrderTraversal(newNode);
+		cbt.writeInOrderTraversal(newNode);
 
+		// This doesn't work.
 		System.out.println("Printing inorder non-recursive traversal of copy:");
 		cbt.inOrderNonRecursiveTraversal(newNode);
 	}
